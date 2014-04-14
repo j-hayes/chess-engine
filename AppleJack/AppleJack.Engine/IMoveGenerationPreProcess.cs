97a3733d7cd01c3420363243329baf0aedf3198a
jackson hayes <jhayes2118@gmail.com>
2014-04-14 16:19:31 -0500

    add forgotten files

--- AppleJack/AppleJack.Engine/IMoveGenerationPreProcess.cs
+++ AppleJack/AppleJack.Engine/IMoveGenerationPreProcess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,64 @@ using System.Threading.Tasks;
 
 namespace AppleJack.Engine
 {
-    class MoveGenerationPreProcess
+    public interface IMoveGenerationPreProcess
     {
+        int[][][][] GetPreProcessedLegalMoves();
+
     }
+
+    public class MoveGenerationPreProcess : IMoveGenerationPreProcess
+    {
+
+        /*
+         
+         * Move Ray Creation Rules 
+         * 
+         * Start by moving positively by the number of squares a piece moves
+         * EG king queen rook move one space on the board 0 -> 1 
+         * Knight moves 0 -> 10
+         * 
+         * follow clockwise from that move
+         * 
+         * 
+         * 
+         */
+
+        private int[][][][] MoveDatabase;
+        public MoveGenerationPreProcess()
+        {
+            MoveDatabase = CreateJaggedArray<int[][][][]>(5,64,8,9); 
+            //todo can we make some space memory saving?? a lot of wasted space to accomodate king queen and bishop long move
+        }
+
+        public int[][][][] GetPreProcessedLegalMoves()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        static T CreateJaggedArray<T>(params int[] lengths)
+        {
+            return (T)InitializeJaggedArray(typeof(T).GetElementType(), 0, lengths);
+        }
+
+        static object InitializeJaggedArray(Type type, int index, int[] lengths)
+        {
+            Array array = Array.CreateInstance(type, lengths[index]);
+            Type elementType = type.GetElementType();
+
+            if (elementType != null)
+            {
+                for (int i = 0; i < lengths[index]; i++)
+                {
+                    array.SetValue(
+                        InitializeJaggedArray(elementType, index + 1, lengths), i);
+                }
+            }
+
+            return array;
+        }
+    }
+
+
 }
---
+++ AppleJack/AppleJack.WPF/AppleJack.Engine.Test/TestPreProcessedMoveDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppleJack.Engine.Test
+{
+    [TestClass]
+    public class TestPreProcessedMoveDatabase
+    {
+        private IMoveGenerationPreProcess preProcess;
+        private int[][][][] database;
+        [TestInitialize]
+        public void Before()
+        {
+            preProcess = new MoveGenerationPreProcess();
+            database = preProcess.GetPreProcessedLegalMoves();
+        }
+
+        [TestCleanup]
+        public void After()
+        {
+
+        }
+        
+
+        [TestMethod]
+        public void TestCreation()
+        {
+            Assert.AreEqual(1, database[0][0][0][0]); // this usage will not be valid later
+        }
+    }
+}
